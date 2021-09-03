201e60fec975e6dbef8e866c9089445363fc6fc5
강성훈 <yolly050101@naver.com>
2021-09-03 09:31:24 +0900

    Route create

---
+++ src/firebase.js
@@ -0,0 +1,12 @@
+import * as firebase from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBh2yHzMDuwogfRF5qjpZfCBKsX-OCP_7o",
+  authDomain: "nwitter-94eb5.firebaseapp.com",
+  projectId: "nwitter-94eb5",
+  storageBucket: "nwitter-94eb5.appspot.com",
+  messagingSenderId: "541161562444",
+  appId: "1:541161562444:web:5398012f16e12c25396fe9",
+};
+
+export default firebase.initializeApp(firebaseConfig);
